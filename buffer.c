17664200c7266fefc5ea09da8b126b42941e1792
marcosp <marcosp@Ubuntu.22.4.lan>
2025-11-04 11:50:00 -0800

    created funciton logic

--- buffer.c
+++ buffer.c
@@ -3,7 +3,30 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+// shared variables
+int buffer[5];
+int in = 0;
+int out = 0;
+
+//synchronization primitives
+pthread_mutex_t mutex;
+pthread_cond_t cond_full;
+pthread_cond_t cond_empty;
+sem_t sem_full;
+sem_t sem_empty;
+
 void insert_request(int request, int sync_type) {
+    if (sync_type == 1) { // Mutex only
+
+    }else if(sync_type == 2){ // Condition Variables
+        
+    }else if(sync_type == 3){ // Semaphores
+
+    }
+    else{
+        printf("Invalud Request");
+        exit(1);
+    }
 
 }
 
---
+++ buffer.h
@@ -0,0 +1,13 @@
+#include <pthread.h>
+#include <semaphore.h>
+
+// synchronization primitives
+extern pthread_mutex_t mutex;
+extern pthread_cond_t cond_full;
+extern pthread_cond_t cond_empty;
+extern sem_t sem_full;
+extern sem_t sem_empty;
+
+// function prototypes
+void insert_request(int request, int sync_type);
+int remove_request(int sync_type);
